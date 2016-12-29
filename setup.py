7ea701a2747506d1e1d4bba421cd0ac2bb964846
Alex Gerdom <alexgerdom@gmail.com>
2016-12-29 16:10:28 -0500

    Got setup.py working

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pdf_processing_utils',
+    version='0.1dev',
+    packages=['pdf_processing_utils',],
+    license='MIT',
+    long_description=open('README.md').read(),
+    author="Alex Gerdom",
+    author_email=""
+)
